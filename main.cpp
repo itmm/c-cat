7272e63f0fd9972bbb050f2bf17c24204b93ce1c
Timm Knape <timm@knp.de>
2021-06-14 21:55:48 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+#include <fstream>
+
+int main(int argc, const char *argv[]) {
+	std::set<std::string> files;
+	for (int i { 1 }; i < argc; ++i) {
+		files.insert(argv[i]);
+	}
+	for (const auto &file : files) {
+		std::cout << "#line 1 " << file << '\n';
+		std::ifstream in { file.c_str() };
+		std::cout << in.rdbuf() << '\n';
+	}
+	return 0;
+}
