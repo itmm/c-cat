864667451bcf5ab59200fab9a07e4e53f47a646e
Timm Knape <timm@knp.de>
2021-06-23 19:05:34 +0200

    remove base directory from line macro

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 
 int main(int argc, const char *argv[]) {
 	std::set<std::string> files;
+	std::string base_dir;
 	for (int i { 1 }; i < argc; ++i) {
-		files.insert(argv[i]);
+		std::string f { argv[i] };
+		if (f.substr(0, 7) == "--base=") {
+			base_dir = f.substr(7);
+		} else {
+			files.insert(f);
+		}
 	}
 	for (const auto &file : files) {
-		std::cout << "#line 1 \"" << file << "\"\n";
+		std::string f { file };
+		if (! base_dir.empty() && f.substr(0, base_dir.size()) == base_dir) {
+			f = f.substr(base_dir.size());
+		}
+		std::cout << "#line 1 \"" << f << "\"\n";
 		std::ifstream in { file.c_str() };
 		std::cout << in.rdbuf() << '\n';
 	}
